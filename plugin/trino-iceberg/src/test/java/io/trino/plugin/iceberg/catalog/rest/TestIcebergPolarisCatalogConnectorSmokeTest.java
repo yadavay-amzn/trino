de35447d54ed3ce85001dbd87e14c80e5894411b
Yuya Ebihara <ebyhry@gmail.com>
2025-09-16 11:01:07 +0900

    Update Iceberg to 1.11.0

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/rest/TestIcebergPolarisCatalogConnectorSmokeTest.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/rest/TestIcebergPolarisCatalogConnectorSmokeTest.java
@@ -219,7 +219,7 @@ final class TestIcebergPolarisCatalogConnectorSmokeTest
     public void testDropTableWithMissingMetadataFile()
     {
         assertThatThrownBy(super::testDropTableWithMissingMetadataFile)
-                .hasMessageMatching(".* Table '.*' does not exist");
+                .hasMessageMatching("Failed to load table: (.*)");
     }
 
     @Test
@@ -243,7 +243,7 @@ final class TestIcebergPolarisCatalogConnectorSmokeTest
     public void testDropTableWithNonExistentTableLocation()
     {
         assertThatThrownBy(super::testDropTableWithNonExistentTableLocation)
-                .hasMessageMatching(".* Table '.*' does not exist");
+                .hasMessageMatching("Failed to load table: (.*)");
     }
 
     @Test
